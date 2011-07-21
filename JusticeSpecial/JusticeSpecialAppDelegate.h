0a13ee2e50042bc83338678920480b0e7a467210
Justin DeWind <dewind@atomicobject.com>
2011-07-21 09:27:59 -0400

    Initial Commit

---
+++ JusticeSpecial/JusticeSpecialAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  JusticeSpecialAppDelegate.h
+//  JusticeSpecial
+//
+//  Created by Justin DeWind on 7/21/11.
+//  Copyright 2011 Atomic Object. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JusticeSpecialAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
